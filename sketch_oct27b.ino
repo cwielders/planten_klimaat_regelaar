d61aae5d1619d827569e354e376b95597b44b57b
Pim van Nierop <pim@thehyve.nl>
2019-11-04 19:14:37 +0100

    Make working implementation for OOD

--- sketch_oct27b.ino
+++ sketch_oct27b.ino
@@ -1,21 +1,9 @@
-void setup() {
-    Serial.begin(9600);
-}
-
-void loop() {
-    delay(1000);
-    A a("Global scope message");
-    B b;
-    b.printMessage();
-}
-
 class A {
 
     String message;
 
     public:
-    A(String myMessage) {
-        message = myMessage;
+    A(String myMessage): message(myMessage) {
     }
 
     void printMessage() {
@@ -26,16 +14,29 @@ class A {
 
 class B {
 
-    // A *a;
+    A a1;
+    A a2;
 
     public:
-    B() {
-        // A myA(message);
-        // a = &myA;
+    B(String message1, String message2) : a1(message1), a2(message2) {
     }
 
     void printMessage() {
-        a.printMessage();
+        a1.printMessage();
+        Serial.print(" === ");
+        a2.printMessage();
+        Serial.print("\n\n");
     }
 
 };
+
+B b("Message_1", "Message_2");
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    delay(1000);
+    b.printMessage();
+}
