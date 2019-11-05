6a706a5ab45ad1df09bf5584be2487045997c3c5
Pim van Nierop <pim@thehyve.nl>
2019-11-05 07:59:12 +0100

    Renamed classes to PlantenBak and Sensor

--- sketch_oct27b.ino
+++ sketch_oct27b.ino
@@ -1,38 +1,38 @@
-class A {
+class Sensor {
 
     String message;                     // private instance variable
 
     public:
-    A(String myMessage):
+    Sensor(String myMessage):
         message(myMessage) {            // init field 'message' using 'myMessage' argument
     }
 
-    void printMessage() {
+    void readvalue() {
         Serial.print(message);
     }
 
 };
 
-class B {
+class PlantenBak {
 
-    A a1;                               // private instance variable
-    A a2;                               // private instance variable
+    Sensor a1;                               // private instance variable
+    Sensor a2;                               // private instance variable
 
     public:
-    B(String message1, String message2): 
+    PlantenBak(String message1, String message2): 
         a1(message1), a2(message2) {    // init fields 'a1' and 'a2' using arguments
     }
 
-    void printMessage() {
-        a1.printMessage();              // will print out 'message1' passed to A instance in B constructor
+    void regelKlimaat() {
+        a1.readvalue();                 // will print out 'message1' passed to A instance in B constructor
         Serial.print(" === ");
-        a2.printMessage();              // will print out 'message2' passed to A instance in B constructor
+        a2.readvalue();                 // will print out 'message2' passed to A instance in B constructor
         Serial.print("\n\n");
     }
 
 };
 
-B b("Message_1", "Message_2");
+PlantenBak bak("Message_1", "Message_2");
 
 void setup() {
     Serial.begin(9600);
@@ -40,5 +40,5 @@ void setup() {
 
 void loop() {
     delay(1000);
-    b.printMessage();
+    bak.regelKlimaat();
 }
